438fdf3516b91d7edf2e70a3baad5d912205c780
Assoghe <67897628+Assoghe@users.noreply.github.com>
2020-07-06 11:37:47 +0200

    Update process.php

--- process.php
+++ process.php
@@ -1,32 +1,50 @@
+
 <?php
-$name = $_POST["name"];
-$email = $_POST["email"];
-$message = $_POST["message"];
- 
-$EmailTo = "soraya.gherras17@gmail.com";
-$Subject = "New Message Received";
- 
-// prepare email body text
-$Body .= "Name: ";
-$Body .= $name;
-$Body .= "\n";
- 
-$Body .= "Email: ";
-$Body .= $email;
-$Body .= "\n";
- 
-$Body .= "Message: ";
-$Body .= $message;
-$Body .= "\n";
- 
-// send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
- 
-// redirect to success page
-if ($success){
-   echo "success";
-}else{
-    echo "invalid";
+
+
+
+$name = stripcslashes($_POST['name']);
+$email = stripcslashes($_POST['email']);
+$subject = stripcslashes($_POST['subject']);
+$message = stripcslashes($_POST['message']);
+
+
+
+$mailto = soraya.gherras17@gmail.com'; 
+$subject = $subject; 
+
+$error_message = 'Une erreur est survenue'; 
+$success_message = 'Message envoyé'; 
+
+
+
+
+$message = "Name: $name\r\nEmail: $email\r\nMessage: $message";
+
+$headers = "From: $name <$email>" . "\r\n" . "Reply-To: $email" . "\r\n" . "X-Mailer: PHP/" . phpversion();
+
+function validateEmail($email) {
+   if(preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $email))
+	  return true;
+   else
+	  return false;
 }
- 
+
+if((strlen($name) < 1 ) || (strlen($email) < 1 ) || (strlen($message) < 1 ) || validateEmail($email) == FALSE ) {
+
+	echo($error_message);
+
+} else {
+
+	if( mail($mailto, $subject, $message, $headers) ) {
+		
+		echo($success_message);
+
+	} else {
+
+		echo($error_message);
+
+	}
+
+}
 ?>
